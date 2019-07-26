49862d90e524a67dd9e3629df08efc0e222093c3
Dave Beshero <dbesher@us.ibm.com>
2019-07-26 16:59:12 -0400

    UX buff: set input placeholder to value

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { forwardRef, useState } from "react";
+import PropTypes from "prop-types";
 import styles from "./Input.module.scss";
 
-const Input = () => {
-  return <input className={styles.root} />;
+const Input = forwardRef((props, ref) => {
+  const { placeholder, type, value, onChange, labelText, ...other } = props;
+  const [currentValue, setCurrentValue] = useState(`${placeholder}${value}`);
+
+  const handleOnChange = e => {
+    setCurrentValue(e.target.value);
+    onChange(e.target.value);
+  };
+
+  return (
+    <input
+      className={styles.root}
+      onChange={handleOnChange}
+      value={currentValue}
+      {...other}
+    />
+  );
+});
+
+Input.propTypes = {
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  labelText: PropTypes.string,
+  type: PropTypes.string,
+  onChange: PropTypes.func
+};
+
+Input.defaultProps = {
+  onChange: () => {},
+  placeholder: "",
+  value: ""
 };
 
 export default Input;
--- src/components/Input/Input.story.js
+++ src/components/Input/Input.story.js
@@ -2,4 +2,6 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Input from "./Input";
 
-storiesOf("Input", module).add("Clean shaven", () => <Input />);
+storiesOf("Input", module)
+  .add("Clean shaven", () => <Input />)
+  .add("With placeholder", () => <Input placeholder="Name" />);
