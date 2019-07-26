f00e34d06069348e0eb25898da96d00e27f929c8
Dave Beshero <dbesher@us.ibm.com>
2019-07-26 18:02:15 -0400

    UX buff: show password by default. When hidden, add two dots per char

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,26 +1,65 @@
-import React, { forwardRef, useState } from "react";
+import React, { forwardRef, useState, Fragment } from "react";
 import PropTypes from "prop-types";
 import styles from "./Input.module.scss";
+import { __values } from "tslib";
 
 const Input = forwardRef((props, ref) => {
-  const { placeholder, type, value, onChange, labelText, ...other } = props;
+  const {
+    placeholder,
+    type,
+    value,
+    onChange,
+    labelText,
+    style,
+    ...other
+  } = props;
   const [currentValue, setCurrentValue] = useState(`${placeholder}${value}`);
+  const [showPassword, setShowPassword] = useState(true);
+  const typeIsPassword = type === "password";
 
   const handleOnChange = e => {
     setCurrentValue(e.target.value);
     onChange(e.target.value);
   };
 
+  const togglePasswordVisibility = () => {
+    console.log(ref);
+    setShowPassword(!showPassword);
+  };
+
+  const passwordIcon = showPassword ? "o__o" : "-__-";
+  const _type = typeIsPassword ? "text" : type;
+
   return (
-    <div className={styles.root}>
+    <div className={styles.root} style={style}>
       <input
         className={styles.input}
         onChange={handleOnChange}
         value={currentValue}
+        type={_type}
         ref={ref}
         {...other}
       />
       {labelText && <label className={styles.label}>{labelText}</label>}
+      {typeIsPassword && (
+        <Fragment>
+          <span
+            className={styles.passwordIcon}
+            onClick={togglePasswordVisibility}
+          >
+            {passwordIcon}
+          </span>
+          {!showPassword && (
+            <span className={styles.hidePassword}>
+              {Object.keys(currentValue)
+                .map(() => {
+                  return "••";
+                })
+                .join("")}
+            </span>
+          )}
+        </Fragment>
+      )}
     </div>
   );
 });
@@ -36,7 +75,8 @@ Input.propTypes = {
 Input.defaultProps = {
   onChange: () => {},
   placeholder: "",
-  value: ""
+  value: "",
+  type: "text"
 };
 
 export default Input;
--- src/components/Input/Input.story.js
+++ src/components/Input/Input.story.js
@@ -2,8 +2,19 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Input from "./Input";
 
+const wrapperStyle = {
+  width: "500px"
+};
+
 storiesOf("Input", module)
-  .add("Clean shaven", () => <Input placeholder="Name" />)
+  .add("Clean shaven", () => <Input placeholder="Name" style={wrapperStyle} />)
   .add("With label", () => (
-    <Input labelText="Full name" placeholder="E.g. Jake M'lady" />
+    <Input
+      labelText="Full name"
+      placeholder="E.g. Jake M'lady"
+      style={wrapperStyle}
+    />
+  ))
+  .add("Password field", () => (
+    <Input labelText="Password" type="password" style={wrapperStyle} />
   ));
