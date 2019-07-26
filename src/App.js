4bc1cc65451168fa2dc15699b3486ed7f93588ab
Dave Beshero <dbesher@us.ibm.com>
2019-07-26 15:27:24 -0400

    initial dir structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.scss";
+import Input from "./components/Input/Input";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.root}>
+      <Input />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Input/Input.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Input.module.scss";
+
+const Input = () => {
+  return <input className={styles.root} />;
+};
+
+export default Input;
